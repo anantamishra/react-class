9d4bec776bea74ebabbdbac2aa82c97be2d58217
anantamishra <mishraananta2017@gmail.com>
2024-09-22 17:20:14 +0545

    init

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export function Contact() {
+  function handelClick() {
+    console.log("Hello!");
+  }
   return (
     <>
       <p>Contact Page</p>
+      <button onClick={handelClick}> Click Me</button>
     </>
   );
 }
