4bd99d3b4f742722682d14da493e0b0ad538ea23
anantamishra <mishraananta2017@gmail.com>
2024-09-25 06:56:30 +0545

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <div>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact</li>
-        </Link>
-      </ul>
+    <div className="flex w-full justify-center bg-white shadow-lg py-6">
+      <div className="flex justify-between w-[90%] ">
+        <div>logo</div>
+        <div>menu</div>
+        <div>button</div>
+      </div>
     </div>
   );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function About() {
   return (
     <div className="text-5xl font-bold">
-      <p className="text-green-900/50">About Page</p>
+      <p className="text-green-900/50 line-thr">About Page</p>
       <p className="text-red-500">About Page</p>
     </div>
   );
