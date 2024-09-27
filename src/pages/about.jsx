5f49961d9dc566b643bfce823d4379ad054e8a49
anantamishra <mishraananta2017@gmail.com>
2024-09-27 10:31:59 +0545

    useState

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function About() {
+  const [name, setName] = useState(0);
+  function sum() {
+    setName(name + 1);
+  }
+  function reset() {
+    setName(0);
+  }
   return (
-    <div className="text-5xl font-bold">
-      <p className="text-green-900/50 line-thr">About Page</p>
-      <p className="text-red-500">About Page</p>
+    <div className="m-5 gap-5  justify-center items-center text-red-400  hidden lg:flex">
+      Value: {name}
+      <button onClick={sum} className="p-2 text-white bg-blue-500 rounded">
+        Increament +
+      </button>
+      <button
+        onClick={reset}
+        className="p-2 text-white bg-red-500 hover:animate-pulse rounded"
+      >
+        Reset
+      </button>
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import { ProfileCard } from "../components";
 
 export function Home() {
+  const datas = [
+    {
+      name: "Ram Sharma",
+      imageUrl: "/logo192.png",
+      description: `Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et`,
+    },
+    {
+      name: "Shyam Sharma",
+      imageUrl: "/logo192.png",
+      description: `Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et`,
+    },
+    {
+      name: "Geeta Sharma",
+      imageUrl: "/logo192.png",
+      description: `Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et`,
+    },
+    {
+      name: "Sita Sharma",
+      imageUrl: "/logo192.png",
+      description: `Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et`,
+    },
+    {
+      name: "Dinesh Sharma",
+      imageUrl: "/logo192.png",
+      description: `Lorem ipsum dolor sit amet, consectetur adip incididunt ut labore et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et dolor. Lorem ipsum dolor sit am et dolor et`,
+    },
+  ];
   return (
-    <>
-      <p>Home Page</p>
-    </>
+    <div className="flex flex-col justify-center items-center gap-4 my-5">
+      {datas.map((data, index) => {
+        return (
+          <ProfileCard
+            key={index}
+            name={data.name}
+            imageUrl={data.imageUrl}
+            description={data.description}
+          />
+        );
+      })}
+    </div>
   );
 }
